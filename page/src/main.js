d5a834c329dbc77b6bfd28f9629bec3193c16ad3
baoq3 <727866412@qq.com>
2020-03-01 14:24:52 +0800

    ant+element+vue

---
+++ page/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugin/ant-design-vue'
+import './plugin/element'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ page/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <el-button type="warning" disabled>警告按钮</el-button>
+    <a-button type="danger">Danger</a-button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
