03047500a6363e4ab79cb87f6e0725867ca9a4bf
baoq3 <727866412@qq.com>
2020-03-26 14:09:17 +0800

    serve 框架

--- serve/index.js
+++ serve/index.js
@@ -1,12 +1,46 @@
 var express = require("express");
-
+var globalConfig = require("./config");
+var loader = require("./loader");
 var app = new express();
+var cookie = require("cookie-parser");
+// var multer = require("multer");
+// var userMsgDao = require("./service/dao/userMsgDao");
 
-app.listen(12345, ()=>{
-    console.log("服务器已启动");
+//绑定静态页面
+// app.use(express.static(globalConfig["page_path"]));
+//使用cookie
+app.use(cookie());
+
+//文件上传配置
+// var uploadSingle = multer({dest:"./file/"});
+
+
+//拦截器,重定向
+app.get("/api/*",( request, response,next )=>{
+    if(request.cookies.id){
+        next();
+    }else{
+        response.writeHead(97);
+        response.write('用户未登录，请先登录');
+        response.end();
+    }
 })
+//自动加载方法
+loader.init(app);
+//上传图片
+// app.post("/upload", uploadSingle.single("file"),(request,response)=>{
+//     userMsgDao.insertUserMsg(request.body.name,request.file.path,request.file.originalname, request.file.size,()=>{
+//         var resp = {
+//             path:request.file.path
+//         }
+//         response.writeHead(200);
+//         response.write(JSON.stringify(resp));
+//         response.end();
+//     });
+// });
 
-
+//监听端口
+app.listen(globalConfig["port"]);
 
 
 
---
+++ serve/loader.js
@@ -0,0 +1,38 @@
+var fs = require("fs");
+var globalConfig = require("./config");
+var multer = require("multer");
+
+//文件上传配置
+var uploadSingle = multer({dest:"./file/"});
+
+var controllerSet = [];
+var pathMap = new Map();
+
+function init(app){
+    var files = fs.readdirSync("./service/" + globalConfig["web_path"]);
+    files.forEach((ei,ii,si)=>{
+        var temp = require("./service/" + globalConfig["web_path"] + "/" + ei);
+        if(temp.path){
+            for(var [key,value] of temp.path){
+                if(pathMap.get(key) == null){
+                    pathMap.set(key,value);
+                    if(value.method == 'GET'){
+                        app.get(key,value.callback);
+                    } else if( value.method == 'File'){
+                        app.post(key, uploadSingle.single("file"), value.callback)
+                    } else{
+                        app.post(key,value.callback);
+                    } 
+                }else{
+                    throw new Error("url path异常，url:" + key);
+                }
+                controllerSet.push(temp);
+            }
+        }
+    });
+}
+
+module.exports.init = init;
+
+
+
