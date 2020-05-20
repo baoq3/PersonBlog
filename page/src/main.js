bd58cb7460519e1835c16577da49056c2d3037d4
baoq3 <727866412@qq.com>
2020-05-20 20:07:21 +0800

    more

--- page/src/main.js
+++ page/src/main.js
@@ -6,11 +6,18 @@ import Antd from 'ant-design-vue'
 import ElementUI from 'element-ui'
 import 'ant-design-vue/dist/antd.less'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import { baseURL } from './enviroment'
 
 Vue.config.productionTip = false
 
 Vue.use(Antd)
 Vue.use(ElementUI)
+Vue.prototype.$http = axios.create({
+  baseURL
+})
+
+axios.defaults.withCredentials = true
 
 new Vue({
   router,
--- page/src/views/Login.vue
+++ page/src/views/Login.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="head">
       <a class="topHeadA" href="">
-        <img class="topImg" src="../assets/logo.png">
+        <img class="topImg" src="../assets/logo.png" />
       </a>
     </div>
     <div class="wrapper">
@@ -15,10 +15,10 @@
                 rules: [
                   {
                     required: true,
-                    message: 'Please input your username!',
-                  },
-                ],
-              },
+                    message: 'Please input your username!'
+                  }
+                ]
+              }
             ]"
             placeholder="请输入用户名"
           />
@@ -31,19 +31,24 @@
                 rules: [
                   {
                     required: true,
-                    message: 'Please input your password!',
+                    message: 'Please input your password!'
                   },
                   {
-                    validator: validateToNextPassword,
+                    validator: validateToNextPassword
                   }
-                ],
-              },
+                ]
+              }
             ]"
             type="new-password"
             placeholder="请输入密码"
           />
         </a-form-item>
-        <a-form-item v-bind="formItemLayout" label="确认密码" has-feedback>
+        <a-form-item
+          v-bind="formItemLayout"
+          label="确认密码"
+          has-feedback
+          v-if="register_show"
+        >
           <a-input
             v-decorator="[
               'confirm',
@@ -51,20 +56,24 @@
                 rules: [
                   {
                     required: true,
-                    message: '请再次输入你的密码!',
+                    message: '请再次输入你的密码!'
                   },
                   {
-                    validator: compareToFirstPassword,
-                  },
-                ],
-              },
+                    validator: compareToFirstPassword
+                  }
+                ]
+              }
             ]"
             type="new-password"
             @blur="handleConfirmBlur"
             placeholder="请再次输入密码"
           />
         </a-form-item>
-        <a-form-item v-bind="formItemLayout" label="手机号">
+        <a-form-item
+          v-bind="formItemLayout"
+          label="手机号"
+          v-if="register_show"
+        >
           <a-input
             v-decorator="[
               'phone',
@@ -72,18 +81,22 @@
                 rules: [
                   {
                     required: true,
-                    message: 'Please input your phone!',
+                    message: 'Please input your phone!'
                   },
                   {
-                    validator: validateToPhone,
-                  },
-                ],
-              },
+                    validator: validateToPhone
+                  }
+                ]
+              }
             ]"
             placeholder="请输入手机号"
           />
         </a-form-item>
-        <a-form-item v-bind="formItemLayout" label="E-mail">
+        <a-form-item
+          v-bind="formItemLayout"
+          label="E-mail"
+          v-if="register_show"
+        >
           <a-input
             v-decorator="[
               'email',
@@ -91,13 +104,23 @@
                 rules: [
                   {
                     required: true,
-                    message: 'Please input your E-mail!',
-                  },
-                ],
-              },
+                    message: 'Please input your E-mail!'
+                  }
+                ]
+              }
             ]"
             placeholder="请输入邮箱"
           />
+        </a-form-item>
+        <a-form-item>
+          <div>
+            <a-button type="primary" html-type="submit">
+              {{ register_show ? "注册" : "登录" }}
+            </a-button>
+            <a-button type="primary" class="register" @click="register">
+              {{ register_show ? "返回" : "注册" }}
+            </a-button>
+          </div>
         </a-form-item>
       </a-form>
     </div>
@@ -105,63 +128,88 @@
 </template>
 
 <script>
+import login from '../api/login'
 export default {
-  data () {
+  data() {
     return {
       confirmDirty: false,
       formItemLayout: {
         labelCol: {
-          xs: { span: 24 },
-          sm: { span: 6, offset: 3 }
+          xs: {
+            span: 24
+          },
+          sm: {
+            span: 6,
+            offset: 4
+          }
         },
         wrapperCol: {
-          xs: { span: 24 },
-          sm: { span: 8 }
+          xs: {
+            span: 24
+          },
+          sm: {
+            span: 6
+          }
         }
-      }
+      },
+      register_show: false // 判断当前是否进入注册页面
     }
   },
 
-  beforeCreate () {
-    this.form = this.$form.createForm(this, { name: 'register' })
+  beforeCreate() {
+    this.form = this.$form.createForm(this, {
+      name: 'Submit'
+    })
   },
 
   methods: {
-    handleSubmit (e) {
+    register(e) {
+      if (!this.$data.register_show) {
+        this.$data.register_show = true
+      } else {
+        this.$data.register_show = false
+      }
+    },
+    handleSubmit(e) {
       e.preventDefault()
       this.form.validateFieldsAndScroll((err, values) => {
         if (!err) {
           console.log('Received values of form: ', values)
+          if (this.register_show) {
+          } else {
+            console.log(login.loginup)
+            login.loginup(values, e => {
+              console.log(e)
+            })
+          }
         }
       })
     },
-    validateToPhone (rule, value, callback) {
-      const form = this.form
+    validateToPhone(rule, value, callback) {
+      // const form = this.form
       if (value && this.confirmDirty) {
-        form.validateFields(['confirm'], { force: true })
+        // form.validateFields(['confirm'], { force: true })
       }
       callback()
     },
-    validateToNextPassword (rule, value, callback) {
-      const form = this.form
+    validateToNextPassword(rule, value, callback) {
+      // const form = this.form
       if (value && this.confirmDirty) {
-        form.validateFields(['confirm'], { force: true })
+        // form.validateFields(['confirm'], { force: true })
       }
       callback()
     },
-    compareToFirstPassword (rule, value, callback) {
+    compareToFirstPassword(rule, value, callback) {
       const form = this.form
       let errorinfo = ''
-      console.log(value)
-      console.log(form.getFieldValue('password'))
       if (value && value !== form.getFieldValue('password')) {
         errorinfo = '两次密码不一致'
       } else {
-        errorinfo = ''
+        errorinfo = undefined // 返回undefined可以使结果出来
       }
       callback(errorinfo)
     },
-    handleConfirmBlur (e) {
+    handleConfirmBlur(e) {
       const value = e.target.value
       this.confirmDirty = this.confirmDirty || !!value
     }
@@ -169,17 +217,18 @@ export default {
 }
 </script>
 
-<style scoped lang='less'>
-
+<style scoped lang="less">
 .head {
   background-color: #171717;
   height: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
+
   .topHeadA {
     color: #337ab7;
     background-color: transparent;
+
     .topImg {
       max-width: 100%;
       border: 0;
@@ -190,7 +239,10 @@ export default {
 }
 
 .wrapper {
-  margin: 20px auto;
+  margin: 80px auto;
 }
 
+.register {
+  margin-left: 5rem;
+}
 </style>
